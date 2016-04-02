6b84667a464f15a571afd25ff31ee0b63cda9aee
Kevin Fitzhenry <kevin.fitzhenry@createthenext.com>
2016-04-02 14:25:09 -0700

    Changed the array formatting

--- catalog.php
+++ catalog.php
@@ -1,4 +1,12 @@
 <?php
+$catalog = array();
+$catalog[201] = "Spotlight";
+$catalog[101] = "Among The Thugs";
+$catalog[202] = "Good Will Hunting";
+$catalog[203] = "Catch Me If You Can";
+$catalog[301] = "The Stone Foxes";
+
+
 $pageTitle = "Full Catalog";
 $section = null;
 
@@ -19,8 +27,17 @@ if (isset($_GET["cat"])) {
 
 include("includes/header.php"); ?>
 
-<div class="section page">
-  <h1><?php echo $pageTitle; ?></h1>
+<div class="section catalog page">
+  <div class="wrapper">
+    <h1><?php echo $pageTitle; ?></h1>
+    <ul>
+      <?php
+      foreach ($catalog as $item) {
+        echo "<li>" . $item . "</li>";
+      }
+       ?>
+    </ul>
+  </div>
 </div>
 
 <?php include("includes/footer.php"); ?>
