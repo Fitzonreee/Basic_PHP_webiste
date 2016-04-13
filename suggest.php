1f757559369341422c4cec9ec71f86940dbc7694
Fitzonreee <kevin.fitzhenry@createthenext.com>
2016-04-12 20:22:37 -0700

    Added two more form inputs: title and category, but they are not appearing in the email

--- suggest.php
+++ suggest.php
@@ -2,11 +2,13 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $name = trim(filter_input(INPUT_POST, "name", FILTER_SANITIZE_STRING));
   $email = trim(filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL));
+  $category = trim(filter_input(INPUT_POST, "category", FILTER_SANITIZE_STRING));
+  $title = trim(filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING));
   $details = trim(filter_input(INPUT_POST, "details", FILTER_SANITIZE_SPECIAL_CHARS));
 
   //Form validations
-  if ($name == "" OR $email == "" OR $details == "") {
-    echo "Please fill in the required fields: Name, Email and Details";
+  if ($name == "" OR $email == "" OR $details == "" OR $category="" OR $title="") {
+    echo "Please fill in the required fields: Name, Email, Title, Category and Details";
     exit;
   }
 
@@ -24,17 +26,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     exit;
   }
 
-  // header redirect must come before this
+  // header redirect must come before this - style this shit and see if it works!
   $email_body = "";
   $email_body .= "Name: " . $name . "\n";
   $email_body .= "Email: " . $email . "\n";
+  $email_body .= "Category: " . $category . "\n";
+  $email_body .= "Title: " . $title . "\n";
   $email_body .= "Details: " . $details;
 
+  // You should absolutely use different email server in production environment ex. Postmark, GmailSMPT
   // PHP Mailer shizzz
   $mail->setFrom($email, $name);
   $mail->addAddress('kevin.fitzhenry@createthenext.com', 'Kevin Fitzhenry'); // Add a recipient
 
-  $mail->isHTML(false); // Set email format to HTML
+  $mail->isHTML(false); // Set email format to HTML - change to true and style!
 
   $mail->Subject = 'New Movie suggestion from ' . $name;
   $mail->Body    = $email_body;
@@ -63,12 +68,25 @@ include("includes/header.php");
     <p>Complete the form to send me an email with a suggestion.</p>
     <form method="post" action="suggest.php">
       <div class="form-group">
-        <label for="name">Name</label>
+        <label for="name">Name<span class="required">Required</span></label>
         <input type="text" name="name" placeholder="First and Last name">
       </div>
       <div class="form-group">
-        <label for="email">Email</label>
+        <label for="email">Email<span class="required">Required</span></label>
         <input type="email" name="email" id="email" placeholder="Valid email address">
+      </div>
+      <div class="form-group">
+        <label for="category">Category<span class="required">Required</span></label>
+        <select name="category">
+          <option value="" selected disabled>Select One</option>
+          <option value="">Book</option>
+          <option value="">Movie</option>
+          <option value="">Music</option>
+        </select>
+      </div>
+      <div class="form-group">
+        <label for="title">Title<span class="required">Required</span></label>
+        <input type="text" name="title" id="title" placeholder="Add the Movie title">
       </div>
       <div class="form-group">
         <label for="details">Suggestion Details</label>
