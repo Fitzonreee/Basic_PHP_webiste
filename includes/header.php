e15112977d2b863429c0c5404492e3850559ad4d
Fitzonreee <kevin.fitzhenry@createthenext.com>
2016-04-15 07:46:22 -0700

    Added Bootstrap and Bootflat css to header.php and Bootstrap and jQuery JS to footer.php

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,8 @@
 <html>
 <head>
 	<title><?php echo $pageTitle; ?></title>
+	<link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://bootflat.github.io/bootflat/css/bootflat.css">
 	<!-- <link rel="stylesheet" href="css/style.css" type="text/css"> -->
 	<link rel="stylesheet" href="dist/css/c.min.css" type="text/css">
 </head>
