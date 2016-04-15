f8441364a17bd9d01824b88cba5d43eb90c52f70
Fitzonreee <kevin.fitzhenry@createthenext.com>
2016-04-15 07:41:28 -0700

    Added gulpfile.js and package.json, up and running with gulp, compiling and minifying sass and js

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,8 @@
 <html>
 <head>
 	<title><?php echo $pageTitle; ?></title>
-	<link rel="stylesheet" href="css/style.css" type="text/css">
+	<!-- <link rel="stylesheet" href="css/style.css" type="text/css"> -->
+	<link rel="stylesheet" href="dist/css/c.min.css" type="text/css">
 </head>
 <body>
 	<div class="header">
